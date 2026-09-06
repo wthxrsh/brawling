83cff816202e66cf635de4de2cce87c9019be636
wthxrsh <wthxrsh@gmail.com>
2026-09-06 21:34:27 +0530

    chore: initialize brawling backend

---
+++ src/main/java/com/brawlstars/brawling/BrawlingApplication.java
@@ -0,0 +1,13 @@
+package com.brawlstars.brawling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrawlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrawlingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brawlstars/brawling/BrawlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brawlstars.brawling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrawlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
